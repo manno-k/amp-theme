51c007a6dba15887db2f74f2538388418b1fde05
manno-k <art.kuromi@gmail.com>
2017-02-28 19:32:23 +0900

    fixed header

--- header-bar.php
+++ header-bar.php
@@ -1,10 +1,14 @@
 <header class="amp-wp-header">
 	<div>
 		<a href="<?php echo esc_url( $this->get( 'home_url' ) ); ?>">
-			<?php $site_icon_url = $this->get( 'site_icon_url' );
+			
+			<!-- ↓ここのコメントアウトを外すとアイコンが表示されます。 -->
+			<!-- <?php $site_icon_url = $this->get( 'site_icon_url' );
 				if ( $site_icon_url ) : ?>
 				<amp-img src="<?php echo esc_url( $site_icon_url ); ?>" width="32" height="32" class="amp-wp-site-icon"></amp-img>
-			<?php endif; ?>
+			<?php endif; ?> -->
+			<!-- ↑ここのコメントアウトを外すとアイコンが表示されます。 -->
+
 			<?php echo esc_html( $this->get( 'blog_name' ) ); ?>
 		</a>
 	</div>
--- sns.php
+++ sns.php
@@ -6,19 +6,19 @@ $title_encode = urlencode(get_the_title());
     <ul>
 			<li>
 					<a class="sns--icon__mail" href="mailto:<?php echo $admin_email = get_option( 'admin_email' ); ?>">
-            <?php $this->load_parts( array( 'mail' ) ); ?>
+            <?php $this->load_parts( array( 'icon-mail' ) ); ?>
 					</a>
 			</li>
         <li>
             <a class="sns--icon__fb"
                href="https://www.facebook.com/sharer.php?src=bm&u=<?php echo $url_encode; ?>&t=<?php echo $title_encode; ?>"
                target="_blank">
-               <?php $this->load_parts( array( 'fb' ) ); ?>
+               <?php $this->load_parts( array( 'icon-fb' ) ); ?>
             </a>
         </li>
         <li>
             <a class="sns--icon__hatena" href="https://b.hatena.ne.jp/add?mode=confirm&url=<?php echo $url_encode ?>">
-              <?php $this->load_parts( array( 'hatena' ) ); ?>
+              <?php $this->load_parts( array( 'icon-hatena' ) ); ?>
         </span>
             </a>
         </li>
@@ -26,7 +26,7 @@ $title_encode = urlencode(get_the_title());
             <a class="sns--icon__twitter"
                href="https://twitter.com/intent/tweet?url=<?php echo $url_encode ?>&text=<?php echo $title_encode ?>&tw_p=tweetbutton"
                target="_blank">
-               <?php $this->load_parts( array( 'tw' ) ); ?>
+               <?php $this->load_parts( array( 'icon-tw' ) ); ?>
             </a>
         </li>
     </ul>
